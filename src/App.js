0d08bb3dc01ab8de4ea1dc3593f2da01d5aca739
umut guven <ggvenumut@gmail.com>
2023-01-07 15:11:28 +0300

    react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  Products,
+  SingleProducts,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<SingleProducts />} />
+        <Route
+          path="checkout"
+          element={
+            <PrivateRoute>
+              <Checkout />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
