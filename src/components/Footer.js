4db075486bc1a70a38ad961df3f85959371d424a
umut guven <ggvenumut@gmail.com>
2023-01-07 15:38:27 +0300

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,16 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 const Footer = () => {
-  return <h4>footer</h4>
-}
+  return (
+    <Wrapper>
+      <h5>
+        &copy; {new Date().getFullYear()}
+        <span>ComfySloth</span>
+      </h5>
+      <h5>All rights reserved</h5>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.footer`
   height: 5rem;
@@ -18,7 +26,6 @@ const Wrapper = styled.footer`
   h5 {
     color: var(--clr-white);
     margin: 0.1rem;
-
     font-weight: 400;
     text-transform: none;
     line-height: 1.25;
@@ -26,6 +33,6 @@ const Wrapper = styled.footer`
   @media (min-width: 776px) {
     flex-direction: row;
   }
-`
+`;
 
-export default Footer
+export default Footer;
