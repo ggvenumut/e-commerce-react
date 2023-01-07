03540170d4c696281047b9e5ba83f7a0cd71f16f
umut guven <ggvenumut@gmail.com>
2023-01-07 17:05:44 +0300

    About Checkout Page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page  section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit
+            voluptatibus quibusdam alias! Rerum, nesciunt veniam quas eos nam
+            dignissimos ex aliquam officia aperiam dolor consequatur saepe earum
+            eum natus? Sed, ex suscipit vitae quisquam voluptatem eaque
+            voluptatibus porro deleniti ducimus, laudantium fuga sapiente
+            libero, enim corporis? Esse laborum praesentium ullam!
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +54,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
