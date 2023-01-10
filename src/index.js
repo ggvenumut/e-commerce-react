11411a0294b8eb268d4b57ec39b2eb293123031c
umut guven <ggvenumut@gmail.com>
2023-01-10 21:15:38 +0300

    Auth0 - Toggle Values

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH_DOMAIN}
