b8c8492f099f86dc27448767944963fff8007e4d
umut guven <ggvenumut@gmail.com>
2023-01-12 16:57:58 +0300

    Auth0 - Hide Checkout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,8 @@ import { useUserContext } from "../context/user_context";
 
 const Nav = () => {
   const { openSidebar } = useProductsContext();
-
+  const { myUser } = useUserContext();
+  console.log(myUser);
   return (
     <NavContainer>
       <div className="nav-center">
@@ -31,6 +32,11 @@ const Nav = () => {
               </li>
             );
           })}
+          {myUser && (
+            <li>
+              <Link to="/checkout">checkout</Link>
+            </li>
+          )}
         </ul>
         <CartButtons />
       </div>
